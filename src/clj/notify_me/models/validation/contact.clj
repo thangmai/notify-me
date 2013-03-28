b2997fad216321b92dd8202c37aadec237937aaf
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-03-28 17:31:11 -0300

    cell phones are validated now of belonging only to ANCEL, prefixes 099 098 and 091

--- src/clj/notify_me/models/validation/contact.clj
+++ src/clj/notify_me/models/validation/contact.clj
@@ -1,23 +1,17 @@
-
 (ns notify-me.models.validation.contact)
 
 (def rules
-  { :name  {:validations
-            {:required true
-              :max-length 100}
-             :messages
-             {:required "El nombre del contacto es requerido."
-              :max-length "El largo del nombre no puede superar los 100 caracteres"}}
-   :type {:validations {
-                        :required true}
-          :mesages {
-                    :required "El tipo de contacto es requerido"}}
-   :cell_phone  {:validations
-                 {:required true
-                  :unique true
-                  :max-length 20}
-                 :messages
-                 {:required "El telefono es requerido."
-                  :unique "El telefono %s ya esta en uso en otro contacto."
-                  :max-length "El largo del telefono no puede superar los 20 caracteres"}}
-   })
+  { :name  {:validations {:required true
+                          :max-length 100}
+            :messages {:required "El nombre del contacto es requerido."
+                        :max-length "El largo del nombre no puede superar los 100 caracteres"}}
+   :type {:validations {:required true}
+          :mesages {:required "El tipo de contacto es requerido"}}
+   :cell_phone  {:validations {:required true
+                               :unique true
+                               :regex #"^(099|091|098)(\d{6})$"
+                               :max-length 20}
+                 :messages {:required "El telefono es requerido."
+                            :unique "El telefono %s ya esta en uso en otro contacto."
+                            :max-length "El largo del telefono no puede superar los 20 caracteres"
+                            :regex "Solamente telefonos de ancel son permitidos, comenzando con 099, 098 y 091"}}})
