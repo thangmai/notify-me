cf7e747569e36b4f411ee7822e7387f9e1f40b85
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-03-28 17:30:32 -0300

    missing validation rules added for policy and trunk entities

--- src/clj/notify_me/models/validation/policy.clj
+++ src/clj/notify_me/models/validation/policy.clj
@@ -1,12 +1,29 @@
 (ns notify-me.models.validation.policy)
 
 (def rules
-  { :name  {:validations
-            {:unique true
-             :required true
-              :max-length 50}
-             :messages
-            {:unique "El nombre %s ya esta en uso"
-             :required "El nombre de las politicas de entrega es requerido."
-              :max-length "El largo del nombre no puede superar los 100 caracteres"}}
-   })
+  {:name  {:validations {:unique true
+                          :required true
+                          :max-length 50}
+            :messages {:unique "El nombre %s ya esta en uso"
+                       :required "El nombre de las politicas de entrega es requerido."
+                       :max-length "El largo del nombre no puede superar los 100 caracteres"}}
+  :retries_on_error {:validations {:required true
+                                   :range [0 5]}
+                     :messages {:required "Los reintentos por error son requeridos"
+                                :range "Los reintentos deben ser un valor entre 0 y 5"}}
+  :retries_on_busy {:validations {:required true
+                                   :range [0 10]}
+                     :messages {:required "Los reintentos por ocupado son requeridos"
+                                :range "Los reintentos deben ser un valor entre 0 y 10"}}
+  :no_answer_retries {:validations {:required true
+                                    :range [0 10]}
+                      :messages {:required "Los reintentos por no atencion son requeridos"
+                                 :range "Los reintentos deben ser un valor entre 0 y 10"}}
+  :no_answer_interval_secs {:validations {:required true
+                                          :range [10 600]}
+                            :messages {:required "Los segundos de reintento por no atencion son requeridos"
+                                       :range "Los segundos de reintento deben ser un valor entre 10 y 600"}}
+  :busy_interval_secs {:validations {:required true
+                                     :range [10 600]}
+                       :messages {:required "Los segundos de reintento por ocupado son requeridos"
+                                  :range "Los segundos de reintento deben ser un valor entre 10 y 600"}}})
--- src/clj/notify_me/models/validation/trunk.clj
+++ src/clj/notify_me/models/validation/trunk.clj
@@ -1,12 +1,51 @@
 (ns notify-me.models.validation.trunk)
 
 (def rules
-  { :name  {:validations
-            {:unique true
-             :required true
-              :max-length 50}
-             :messages
-            {:unique "El nombre %s ya esta en uso"
-             :required "El nombre del troncal es requerido."
-              :max-length "El largo del nombre no puede superar los 50 caracteres"}}
-   })
+  {:name {:validations {:unique true
+                        :required true
+                        :max-length 50}
+          :messages {:unique "El nombre %s ya esta en uso"
+                     :required "El nombre del troncal es requerido."
+                     :max-length "El largo del nombre no puede superar los 50 caracteres"}}
+   :technology {:validations {:required true
+                              :max-length 10}
+                :messages {:required "La tecnologia es requerida"
+                           :max-length "El largo maximo permitido es para la tecnologia 10"}}
+   :number {:validations {:required true
+                          :max-length 20}
+            :messages {:required "El numero del troncal es requerido"
+                       :max-length "El largo maximo permitido para el numero es 20"}}
+   :context {:validations {:required true
+                           :max-length 50}
+             :messages {:required "El nombre del contexto de salida es requerido"
+                        :max-length "El largo maximo permitido para el contexto es 50"}}
+   :extension {:validations {:required true
+                             :max-length 20}
+               :messages {:required "La extension es requerida"
+                          :max-length "El largo maximo permitido para la extension es 10"}}
+   :priority {:validations {:required true
+                            :max-length 20}
+              :messages {:required "La prioridad es requerida"
+                         :max-length "El largo maximo permitido para la prioridad es 20"}}
+   :callerid {:validations {:required true
+                            :max-length 20}
+              :messages {:required "El caller id es requerido"
+                         :max-length "El largo maximo permitido para el caller id es 20"}}
+   :capacity {:validations {:required true
+                            :digits true
+                            :range [1 200]}
+              :messages {:required "La capacidad es requerida"
+                         :digits "La capacidad debe ser un numero"
+                         :range "La capacidad del troncal debe ser un valor entre 1 y 200"}}
+   :host {:validations {:required true
+                        :max-length 100}
+          :messages {:required "El host es requerido"
+                     :max-length "El largo maximo permitido para el host id es 100"}}
+   :user {:validations {:required true
+                        :max-length 50}
+          :messages {:required "El usuario es requerido"
+                     :max-length "El largo maximo permitido para el usuario id es 50"}}
+   :password {:validations {:required true
+                            :max-length 50}
+              :messages {:required "El password es requerido"
+                         :max-length "El largo maximo permitido para el password id es 50"}}})
