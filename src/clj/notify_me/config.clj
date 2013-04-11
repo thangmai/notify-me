80c3f6f353aa3a2256353e2b5d4eed9295efcabf
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-04-11 20:56:43 -0300

    service properties are read from configuration file
    assumed to be located at /etc/notify-me.conf. also are the service default were changed to generic stuff probably not working without a conf file anymore

--- src/clj/notify_me/config.clj
+++ src/clj/notify_me/config.clj
@@ -1,8 +1,24 @@
-(ns notify-me.config)
+(ns notify-me.config
+  (:require [clojure.java.io]
+            [clojure.tools.logging :as log])
+  (:use [slingshot.slingshot :only [try+ throw+]]))
 
-(def opts {:port 5000
-           :host "localhost"
-           :user "guille"
-           :password "bogan731"
-           :temp-dir "/tmp"
-           :tts-command "/Users/guilespi/Documents/Development/interrupted/google-tts/text2mp3.sh"})
+(def default-opts {:port 5000
+                   :host "localhost"
+                   :user "user"
+                   :password "password"
+                   :temp-dir "/tmp"
+                   :tts-command "text2mp3.sh"})
+
+(defn read-properties
+  [file-name]
+  (try+ 
+   (with-open [^java.io.Reader reader (clojure.java.io/reader file-name)] 
+     (let [props (java.util.Properties.)]
+       (.load props reader)
+       (into {} (for [[k v] props] [(keyword k) (read-string v)]))))
+   (catch Object _
+     (log/warn "Unable to read properties file" file-name)
+     {})))
+
+(def opts (merge default-opts (read-properties "/etc/notify-me.conf")))
