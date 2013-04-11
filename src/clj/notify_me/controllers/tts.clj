c22bcc70bdc36c2bbb5aafde1449539ca9c3d775
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-04-11 18:59:29 -0300

    fixed mime-type for tts audio not supported by mozilla
    the standard mime-type audio/vnd.wave was raising an unsupported error in mozilla browser

--- src/clj/notify_me/controllers/tts.clj
+++ src/clj/notify_me/controllers/tts.clj
@@ -14,7 +14,7 @@
     (if (and (= exit 0) (-> filename empty? not))
       (->
        (response/file-response filename {:root (:temp-dir config/opts)})
-       (response/content-type "audio/vnd.wave")
+       (response/content-type "audio/wav")
        (response/header "Content-Disposition" (format "inline; filename='%s'" filename)))
       {:status 500 :body (:err response)})))
 
