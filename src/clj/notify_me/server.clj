097647e21ffbeed14e8034d029024c52df58f7ea
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-04-12 22:27:13 -0300

    Refactored application startup code to use Lifecycle approach.
    As seen in "clojure in the large" talk by Stuart Sierra, mostly development helpers to reload without restarting

--- src/clj/notify_me/server.clj
+++ src/clj/notify_me/server.clj
@@ -7,12 +7,47 @@
    [clj-logging-config.log4j :as log-config]
    [clojure.tools.logging :as log]))
 
+(defprotocol Lifecycle
+  (start [this] "Start all life")
+  (stop [this] "End all life"))
+
+(defrecord QuartzJobs []
+  Lifecycle
+  (start [_]
+    (qs/initialize)
+    (qs/start)
+    (qs/schedule (notifier/job) (notifier/trigger)))
+  (stop [_]
+    (qs/unschedule-job (notifier/id))
+    (qs/shutdown)))
+
+(defrecord JettyWeb []
+  Lifecycle
+  (start [this]
+    (webserver/start))
+  (stop [this]
+    (webserver/stop)))
+
+(defrecord App [job-manager web-service]
+  Lifecycle
+  (start [_]
+    (log/info "Starting Application")
+    (start job-manager)
+    (start web-service))
+  (stop [_]
+    (log/info "Shutting Down Application")
+    (stop job-manager)
+    (stop web-service)))
+
+(defn prod-system []
+  (let [web-service (->JettyWeb)
+        job-manager (->QuartzJobs)]
+    (->App job-manager web-service)))
+
 (defn -main
   [& args]
-  (log/info "Starting")
-  (qs/initialize)
-  (qs/start)
-  (qs/unschedule-job (notifier/id))
-  (qs/schedule (notifier/job) (notifier/trigger))
-  (webserver/start [])
-  (qs/shutdown))
+  (when-let [s (prod-system)]
+    (start s)
+    (.. (Runtime/getRuntime) (addShutdownHook (proxy [Thread] []
+                                                (run []
+                                                  (stop s)))))))
--- src/clj/notify_me/webserver.clj
+++ src/clj/notify_me/webserver.clj
@@ -1,11 +1,17 @@
 (ns notify-me.webserver
   (:require
    [notify-me.config :as config]
-   [compojure.core :as compojure]
    [ring.adapter.jetty :as jetty]
    [notify-me.routes :as routes]))
 
+(def server (atom nil))
 
-(defn start
-  [opts]
-    (jetty/run-jetty routes/app {:join? true :port (:port config/opts)}))
+(defn stop []
+  (when @server
+    (.stop @server)))
+
+(defn start []
+  (stop)
+  (when-let [s (jetty/run-jetty routes/app 
+                                {:join? false :port (:port config/opts)})]
+    (reset! server s)))
