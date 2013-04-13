ccab61bb42f70b72057cb714b27373bb03d520a8
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-04-12 23:02:31 -0300

    changed pattern matching for tts translation url
    now supports everything but ?, which in order to be supported tts strategy should be changed

--- src/clj/notify_me/controllers/tts.clj
+++ src/clj/notify_me/controllers/tts.clj
@@ -1,13 +1,15 @@
 (ns notify-me.controllers.tts
   (:require
    [notify-me.config :as config]
-   [ring.util.response :as response])
+   [ring.util.response :as response]
+   [clojure.tools.logging :as log])
   (:use
    [clojure.java.shell :only [sh]]
    [compojure.core :only [defroutes GET POST]]))
 
 (defn- translate
   [text]
+  (log/info (format "Translating to tts '%s'" text))
   (let [response (sh (:tts-command config/opts) (:temp-dir config/opts) text)
         exit (:exit response)
         filename (clojure.string/trim-newline (:out response))]
@@ -19,4 +21,4 @@
       {:status 500 :body (:err response)})))
 
 (defroutes routes
-  (GET "/:text/translate.wav" [text] (translate text)))
+  (GET ["/:text/translate.wav", :text #"[^/]+"] [text] (translate text)))
